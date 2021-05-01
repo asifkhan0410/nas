3b1dd16fc8d4337f5f496214b546b7c8994d1a5f
Asif <asifkhan0410@gmail.com>
2021-05-02 00:31:23 +0530

    added past & new button with global font

--- src/components/StoriesContent.js
+++ src/components/StoriesContent.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './StoriesContent.css'
 
 function StoriesContent() {
     return (
-        <div>
-           Hello 
+        <div className="storiesContent">
+           <header>
+               <button className='storiesContent__new'>New</button>
+               <button className='storiesContent__past'>Past</button>
+           </header>
+
         </div>
     )
 }
